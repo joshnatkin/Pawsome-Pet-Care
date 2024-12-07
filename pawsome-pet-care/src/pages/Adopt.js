9a76be62e146075d3afbfddee7793d2673698c90
joshnatkin <jnatkin@email.sc.edu>
2024-12-06 23:43:21 -0500

    f

--- pawsome-pet-care/src/pages/Adopt.js
+++ pawsome-pet-care/src/pages/Adopt.js
@@ -23,7 +23,7 @@ const Adopt = () => {
 
   const fetchDogs = async () => {
     try {
-      const response = await axios.get("http://localhost:3001/api/dogs/");
+      const response = await axios.get("https://serverside-9phk.onrender.com/api/dogs/");
       setDogs(response.data);
     } catch (error) {
       console.error("Error fetching dogs:", error);
